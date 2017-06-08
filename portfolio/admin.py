8e8dd4213042b362acf5f1bd090704843afeb2eb
Anupam-dagar <dagaranupam@gmail.com>
2017-06-08 12:17:20 +0530

    Deleted one unnecessary file and one line in admin.py

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -3,6 +3,5 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from .models import Portfolio
-from django.contrib.auth.models import Group
 
 admin.site.register(Portfolio)
