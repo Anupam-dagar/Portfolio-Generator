271900998a9f445ad197b2f21ae44e605ddbd116
Anupam-dagar <dagaranupam@gmail.com>
2017-06-26 00:58:31 +0530

    try heroku once more

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
-
+from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
