abc255fdcb14e93ea2efca25ba6f371c5956463c
Anupam-dagar <dagaranupam@gmail.com>
2017-06-27 03:05:48 +0530

    changed admin link

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 urlpatterns = [
 	url(r'', include('portfolio.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^adminsitehnox/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
 ]
