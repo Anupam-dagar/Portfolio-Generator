887115ae011c5b823b09b64f6b4dc5fe2eba37f1
Anupam-dagar <dagaranupam@gmail.com>
2017-07-03 16:51:24 +0530

    tried something

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
 	url(r'^enter-details/$', views.Portfolio_detail, name='Portfolio_detail'),
 	url(r'^(?P<username>[\w.@+-]+)/(?P<pk>[0-9]+)/$', views.Portfolio_display, name='Portfolio_display'),
 	url(r'^your-portfolios/$', views.View_all, name="View_all"),
-	url(r'^(?P<username>[\w.@+-]+)/(?P<pk>[0-9]+)/edit/$', views.editform, name="editform")
+	url(r'^(?P<username>[\w.@+-]+)/(?P<pk>[0-9]+)/edit/$', views.editform, name="editform"),
 ]
